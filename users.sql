47dc8a903b100a4f72abe938c2f9c1032e4c4802
Gvidas <dreamstep@gmail.com>
2016-01-18 21:11:00 +0200

    sql increment & collation fixes

--- users.sql
+++ users.sql
@@ -15,14 +15,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) NOT NULL,
-  `email` varchar(255) COLLATE utf8_lithuanian_ci NOT NULL,
-  `password` varchar(256) COLLATE utf8_lithuanian_ci NOT NULL,
-  `remember_token` varchar(256) COLLATE utf8_lithuanian_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(256) COLLATE utf8_general_ci NOT NULL,
+  `remember_token` varchar(256) COLLATE utf8_general_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `role` int(1) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_lithuanian_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -42,7 +42,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
